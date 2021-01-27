cc36dc83abdfe3c4bdd569db99ab2dbcfa0ec1b1
Paul Onteri <paul@inuua.net>
2021-01-27 08:53:36 +0300

    convert into PWA

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,40 @@
-import { Provider } from 'react-redux'
-import { useStore } from '../store'
+import Head from "next/head";
+import { Provider } from "react-redux";
+import { useStore } from "../store";
 
 export default function App({ Component, pageProps }) {
-  const store = useStore(pageProps.initialReduxState)
+  const store = useStore(pageProps.initialReduxState);
 
   return (
     <Provider store={store}>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+        <meta name="description" content="Description" />
+        <meta name="keywords" content="Keywords" />
+        <title>Next.js PWA Example</title>
+
+        <link rel="manifest" href="/manifest.json" />
+        <link
+          href="/icons/favicon-16x16.png"
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+        />
+        <link
+          href="/icons/favicon-32x32.png"
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+        />
+        <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+        <meta name="theme-color" content="#317EFB" />
+      </Head>
       <Component {...pageProps} />
     </Provider>
-  )
+  );
 }
